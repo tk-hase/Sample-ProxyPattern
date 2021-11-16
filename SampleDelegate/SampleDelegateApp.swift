1239032a72c3f75cd10e4bc903240e8579cc488c
Tomoki Hasegawa <t-hase@nttr-tech.co.jp>
2021-11-16 14:46:50 +0900

    Initial Commit

---
+++ SampleDelegate/SampleDelegateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleDelegateApp.swift
+//  SampleDelegate
+//
+//  Created by Tomoki Hasegawa on 2021/11/16.
+//
+
+import SwiftUI
+
+@main
+struct SampleDelegateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
